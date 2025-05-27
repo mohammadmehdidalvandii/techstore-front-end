2d734d290d42370caf631c686ca91a2dab093b79
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-05-27 16:32:26 +0330

    fixed style navbar

--- src/components/module/Navbar/Navbar.tsx
+++ src/components/module/Navbar/Navbar.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Navbar.css';
 import { NavLink } from 'react-router-dom';
-import { FaSearch , FaShoppingCart , FaUser } from "react-icons/fa";
+import { FaBars, FaSearch , FaShoppingCart , FaTimes, FaUser } from "react-icons/fa";
 
 
 const Navbar:React.FC = ()=>{
+    const [open, setOpen] = React.useState<boolean>(false);
+
+    const handlerMenu:React.MouseEventHandler = ()=>{
+        setOpen((open)=>!open)
+    }
   return (
     <nav className='navbar'>
         <div className="container">
@@ -32,6 +37,47 @@ const Navbar:React.FC = ()=>{
                     <NavLink to='/' className='nav_link'><FaShoppingCart /></NavLink>
                     <NavLink to='/' className='nav_link'><FaUser/></NavLink>
                 </div>
+            </div>
+            <div className="mobile">
+                <div className='menu'>
+                    <NavLink to='/' className='nav_link'><FaSearch/></NavLink>
+                    <NavLink to='/' className='nav_link'><FaShoppingCart /></NavLink>
+                    <NavLink to='/' className='nav_link'><FaUser/></NavLink>
+                </div>
+                <NavLink to='/' className='logo'>TechStore</NavLink>
+                <span className="mobile_icon" onClick={handlerMenu}>
+                    {open ? (
+                        <FaTimes/>
+                    ) :(
+                        <FaBars/>
+                    )}
+                </span>
+                {open && (
+                    <div className="mobile_menu">
+                    <div className='menu'>
+                    <NavLink to='/' className='nav_link'><FaSearch/></NavLink>
+                    <NavLink to='/' className='nav_link'><FaShoppingCart /></NavLink>
+                    <NavLink to='/' className='nav_link'><FaUser/></NavLink>
+                </div>
+                  <ul className="items">
+                    <li>
+                        <NavLink to='' className='nav_link'>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='' className='nav_link'>Products</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='' className='nav_link'>Categories</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='' className='nav_link'>Deals</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='' className='nav_link'>Contact</NavLink>
+                    </li>
+                </ul>
+                </div>
+                )}
             </div>
         </div>
     </nav>
