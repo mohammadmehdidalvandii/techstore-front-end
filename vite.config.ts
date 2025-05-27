648e38fba5f8a5e1570c5f0e82928fc3adf257bf
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-05-27 15:24:55 +0330

    fixed tsConfing and viteConfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@page': '/src/page',
+      '@components': '/src/components',
+      '@components/module': '/src/components/module/',
+      '@components/template': '/src/components/template/',
+      '@utils': 'src/utils',
+      '@hooks': 'src/hooks',
+    },
+  },
+});
