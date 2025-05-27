80a1400e0c20f16b8fc4797b766260c21ff11c81
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-05-27 15:25:32 +0330

    create routes page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { useRoutes } from "react-router-dom"
+import routes from "../src/route"
+
 
 export default function App() {
+  const router = useRoutes(routes)
   return (
-    <div>App</div>
+    <>
+    {router}
+    </>
   )
 }
