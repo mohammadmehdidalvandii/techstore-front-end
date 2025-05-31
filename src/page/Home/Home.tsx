b819cf17d6e13ab096b5833c031265e189b6b637
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-05-31 13:51:07 +0330

    change routing Hero

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '@components/module/Navbar/Navbar'
-import Hero from '@components/template/Hero/Hero'
+import Hero from '@components/template/home/Hero/Hero'
 
 const Home:React.FC = ()=>{
   return (
