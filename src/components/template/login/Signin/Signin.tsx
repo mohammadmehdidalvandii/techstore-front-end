1cb53bdaaa19d3cce0cfc23d205700d269a7430c
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-06-10 14:40:37 +0330

    create section signin for login-page

---
+++ src/components/template/login/Signin/Signin.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Signin.css';
+import { NavLink } from 'react-router-dom';
+
+const Signin:React.FC = ()=>{
+  return (
+    <section className="signin">
+        <div className="wrapper">
+            <h4 className="title">Welcome Back  </h4>
+            <p className="text">Sign in to your account</p>
+        </div>
+        <form action="#" className="form">
+            <div className="group_form">
+                <label htmlFor="" className="label_form">Email Address</label>
+                <input type="text" className="input_form" placeholder='example@gmail.com' />
+            </div>
+            <div className="group_form">
+                <label htmlFor="" className="label_form">Password</label>
+                <input type="text" className="input_form"  placeholder='**********'/>
+            </div>
+                <NavLink to='/' className='signin_forget'>Forgot Password?</NavLink>
+                <button className="btn primary btn_signin">Signin</button>
+                <p className='form_account'>Don't have an account?
+                    <NavLink to='/' className='link_signup'>Sign Up</NavLink>
+                </p>
+        </form>
+    </section>
+  )
+}
+
+export default Signin
--- src/page/Login/Login.tsx
+++ src/page/Login/Login.tsx
@@ -1,11 +1,13 @@
 import Footer from '@components/module/Footer/Footer'
 import Navbar from '@components/module/Navbar/Navbar'
+import Signin from '@components/template/login/Signin/Signin'
 import React from 'react'
 
 const Login:React.FC = ()=>{
   return (
     <>
     <Navbar/>
+    <Signin/>
     <Footer/>
     </>
   )
