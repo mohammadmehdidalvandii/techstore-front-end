49aed88e0dad143a4f2ab4c72cf379a4b94785a6
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-07-05 14:55:37 +0330

    create userPane-nav

---
+++ src/components/template/user-panel/UserPanelNav/UserPanelNav.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './UserPanelNav.css'
+import { Link } from 'react-router-dom';
+
+const UserPanelNav:React.FC = ()=>{
+  return (
+    <div className='UserPanelNav'>
+        <div className="userPanel_info">
+            <img src="/images/user.jpg" alt="" />
+            <h6>John Doe</h6>
+            <span>john.doe@example.com</span>
+        </div>
+        <ul className="userPanel_menu">
+            <li>
+                <Link to='/UserPanel/Overview' className='link active'>Overview</Link>
+            </li>
+            <li>
+                <Link to='/UserPanel/Orders' className='link'>Orders</Link>
+            </li>
+            <li>
+                <Link to='/UserPanel/Wishlist' className='link'>Wishlist</Link>
+            </li>
+            <li>
+                <Link to='/UserPanel/Address' className='link'>Address</Link>
+            </li>
+            <li>
+                <Link to='/UserPanel/Payment' className='link'>Payment Methods</Link>
+            </li>
+            <li>
+                <Link to='/UserPanel/Setting' className='link'>Setting</Link>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default UserPanelNav
--- src/page/User-panel/User-panel.tsx
+++ src/page/User-panel/User-panel.tsx
@@ -1,5 +1,6 @@
 import Footer from '@components/module/Footer/Footer'
 import Navbar from '@components/module/Navbar/Navbar'
+import UserPanelNav from '@components/template/user-panel/UserPanelNav/UserPanelNav'
 import React from 'react'
 
 const UserPanel:React.FC = ()=>{
@@ -7,7 +8,9 @@ const UserPanel:React.FC = ()=>{
     <>
     <Navbar/>
      <div className="container_userPanel container">
-        <div className="userPanel_nav box"></div>
+        <div className="userPanel_nav box">
+            <UserPanelNav/>
+        </div>
         <div className="userPanel_content box"></div>
      </div>
     <Footer/>
