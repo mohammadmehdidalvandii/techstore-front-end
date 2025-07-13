4af2c2d585e376ce5c202e06e509c9be80f2717f
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-07-13 16:06:23 +0330

    create section sidebar for admin/panel

---
+++ src/components/template/admin-panel/Sidebar/Sidebar.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import './Sidebar.css';
+import { NavLink } from 'react-router-dom';
+import { FaBox, FaHome, FaShoppingCart, FaStar, FaTag, FaUsers } from 'react-icons/fa';
+import { MdSettings } from 'react-icons/md';
+
+const Sidebar:React.FC = ()=>{
+  return (
+    <section className="sidebar">
+        <div className="sidebar_wrapper">
+            <ul className="sidebar_items">
+                <li className="item">
+                    <NavLink to='/Admin' className='sidebar_link sidebar_link_active '>
+                        <span className="icon"><FaHome/></span>
+                        <span className="text">Dashboard</span>
+                    </NavLink>
+                </li>
+                <li className="item">
+                    <NavLink to='/Admin/' className='sidebar_link'>
+                        <span className="icon"><FaBox/></span>
+                        <span className="text">Products</span>
+                    </NavLink>
+                </li>
+                <li className="item">
+                    <NavLink to='/Admin/' className='sidebar_link'>
+                        <span className="icon"><FaShoppingCart/></span>
+                        <span className="text">Orders</span>
+                    </NavLink>
+                </li>
+                <li className="item">
+                    <NavLink to='/Admin/' className='sidebar_link'>
+                        <span className="icon"><FaUsers/></span>
+                        <span className="text">Customers</span>
+                    </NavLink>
+                </li>
+                <li className="item">
+                    <NavLink to='/Admin/' className='sidebar_link'>
+                        <span className="icon"><FaTag/></span>
+                        <span className="text">Categories</span>
+                    </NavLink>
+                </li>
+                <li className="item">
+                    <NavLink to='/Admin/' className='sidebar_link'>
+                        <span className="icon"><FaStar/></span>
+                        <span className="text">Reviews</span>
+                    </NavLink>
+                </li>
+                <li className="item">
+                    <NavLink to='/Admin/' className='sidebar_link'>
+                        <span className="icon"><MdSettings/></span>
+                        <span className="text">Settings</span>
+                    </NavLink>
+                </li>
+            </ul>
+        </div>
+    </section>
+  )
+}
+
+export default Sidebar
--- src/page/Admin/Admin.tsx
+++ src/page/Admin/Admin.tsx
@@ -1,9 +1,12 @@
+import Sidebar from '@components/template/admin-panel/Sidebar/Sidebar'
 import React from 'react'
 
 const Admin:React.FC = ()=>{
   return (
     <div className="container_admin">
-        <div className="admin_sidebar">this is sidebar</div>
+        <div className="admin_sidebar">
+            <Sidebar/>
+        </div>
         <div className="admin_content">this is content admin</div>
     </div>
   )
